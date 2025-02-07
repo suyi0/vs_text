f2becd24efec7bff4e50b4465bd614765353b93f
suyi0 <AIsuqun@outlook.com>
2025-02-07 21:33:37 +0800

    new

--- text_2025.2.2/源.cpp
+++ text_2025.2.2/源.cpp
@@ -12,11 +12,24 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	const char* a[] = { "work","at","alibaba" };
+//	const char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);
+//	return 0;
+//}
+
 int main()
 {
-	const char* a[] = { "work","at","alibaba" };
-	const char** pa = a;
-	pa++;
-	printf("%s\n", *pa);
+	const char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	const char** cp[] = { c + 3,c + 2,c + 1,c };
+	const char*** cpp = cp;
+
+	printf("%s\n", **++cpp);
+	printf("%s\n", *--*++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][+1] + 1);
 	return 0;
 }
